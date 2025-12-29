e462979431ee0df59ae191723a9d43c0a6ebcdf4
CHINAMI-VT3PCAR\Administrator <dongrui@dongrui.com>
2025-12-29 17:09:44 +0800

    提交

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-    // 纯静态站点不需要 adapter，Cloudflare Pages 会自动托管 dist 目录
-    output: 'static',
+
 });
